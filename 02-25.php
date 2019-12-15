55f33842d56986ffb0f7f089ccc12490a76a6bee
The Maya Hiker <the.maya.hiker@gmail.com>
2019-12-15 13:14:42 +0900

    Sample of array.

---
+++ 02-25.php
@@ -0,0 +1,8 @@
+<?php
+// 3つの要素を持つ連想配列を作成する
+$maximum_version = array("PHP3" => "3.0.18",
+                         "PHP4" => "4.4.8",
+												 "PHP5" => "5.2.6");
+
+// 配列の要素数を取得して表示する
+echo count($maximum_version);
