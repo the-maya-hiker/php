697337b346fc4dc8763bea45dcb7ae7c2f370216
The Maya Hiker <the.maya.hiker@gmail.com>
2019-12-15 13:14:34 +0900

    Sample of array.

---
+++ 02-24.php
@@ -0,0 +1,11 @@
+<?php
+// 空の配列を作成する
+$arr = array();
+
+// 3つの要素を持つ添字配列を作成する
+$php = array("PHP3", "PHP4", "PHP5");
+$php_version = array(3 => "PHP3", 4 => "PHP4", 5 => "PHP5");
+
+// それぞれの配列の要素数を取得して表示する
+echo count($php);
+echo count($php_version);
